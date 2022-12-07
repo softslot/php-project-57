02b715d4a5016551e4e4262eb7c88f488f928e69
Konstantin Korolenko <softslot-com@yandex.ru>
2022-12-08 00:49:04 +0300

    add new translate

--- lang/en/flash.php
+++ lang/en/flash.php
@@ -4,4 +4,5 @@ return [
     'status_added' => 'Status successfully added',
     'status_edited' => 'Status successfully changed',
     'status_deleted' => 'Status successfully deleted',
+    'status_not_deleted' => 'Failed to delete status',
 ];
--- lang/ru/flash.php
+++ lang/ru/flash.php
@@ -4,4 +4,5 @@ return [
     'status_added' => 'Статус успешно создан',
     'status_edited' => 'Статус успешно изменён',
     'status_deleted' => 'Статус успешно удалён',
+    'status_not_deleted' => 'Не удалось удалить статус',
 ];
