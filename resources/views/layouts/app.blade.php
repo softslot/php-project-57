191d49946bcd3c48264458575b77fc4755c6b240
Konstantin Korolenko <softslot-com@yandex.ru>
2022-12-07 14:02:11 +0300

    refactoring layout/app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="csrf-param" content="_token" />
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ __('main.project_name') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -13,23 +14,67 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <body>
+        <div id="app">
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+            <header class="fixed w-full">
+                <nav class="bg-white border-gray-200 py-2.5 dark:bg-gray-900 shadow-md">
+                    <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
+                        <a href="{{ route('home') }}" class="flex items-center">
+                            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">{{ __('main.project_name') }}</span>
+                        </a>
+
+                        <div class="flex items-center lg:order-2">
+                            @if(auth()->guest())
+                            <a href="{{ route('login') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                {{ __('main.login') }}
+                            </a>
+                            <a href="{{ route('register') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">
+                                {{ __('main.register') }}
+                            </a>
+                            @else
+                            <a href="{{ route('logout') }}"
+                               data-method="POST"
+                               class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">
+                                Выход
+                            </a>
+                            @endif
+
+{{--                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">--}}
+{{--                            @csrf--}}
+{{--                            </form>--}}
+                        </div>
+
+                        <div class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1">
+                            <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                                <li>
+                                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
+                                        {{ __('main.tasks') }}
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('task_statuses.index') }}" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
+                                        {{ __('main.statuses') }}
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
+                                        {{ __('main.tags') }}
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </header>
-            @endif
+                </nav>
+            </header>
 
             <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            <section class="bg-white dark:bg-gray-900">
+                <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
+                    @include('flash::message')
+                    {{ $slot }}
+                </div>
+            </section>
         </div>
     </body>
 </html>
