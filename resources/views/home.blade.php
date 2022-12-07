267013650ef29a5f4e7291556e6b26e2684b7797
Konstantin Korolenko <softslot-com@yandex.ru>
2022-12-07 14:00:16 +0300

    add home page view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <div class="mr-auto place-self-center lg:col-span-7">
+        <h1 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
+            {{ __('home_page.h1') }}
+        </h1>
+        <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+            {{ __('home_page.description') }}
+        </p>
+    </div>
+</x-app-layout>
