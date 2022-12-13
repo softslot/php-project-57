87ea31682e2d9dc7d535dcefecdb93c50d19e81a
Konstantin Korolenko <softslot-com@yandex.ru>
2022-12-13 13:59:08 +0300

    modify migration create_tasks_table

--- database/migrations/2022_12_07_221015_create_tasks_table.php
+++ database/migrations/2022_12_07_221015_create_tasks_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('description');
+            $table->string('name')->unique();
+            $table->text('description')->nullable();
             $table->foreignId('status_id')->constrained('task_statuses');
             $table->foreignId('created_by_id')->constrained('users');
             $table->foreignId('assigned_to_id')->nullable()->constrained('users');
