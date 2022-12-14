4c94e79cc816196fb2610977fcedfc331f099c2b
Konstantin Korolenko <softslot-com@yandex.ru>
2022-12-14 13:59:56 +0300

    refactoring migration create_labels_table

--- database/migrations/2022_12_11_093605_create_labels_table.php
+++ database/migrations/2022_12_11_093605_create_labels_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('labels', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->text('description')->nullable();
             $table->timestamps();
         });
