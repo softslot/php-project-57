77601101b05552e66e9f876a835a2ffac9ed261b
Konstantin Korolenko <softslot-com@yandex.ru>
2023-01-26 18:34:45 +0300

    add account traslation

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -6,6 +6,14 @@ return [
     'register' => 'Register',
     'exit' => 'Log out',
 
+    'name' => 'Name',
+    'email' => 'Email',
+    'enter_password' => 'Password',
+    'confirm_password' => 'Confirm Password',
+    'already_registered' => 'Already registered?',
+    'remember' => 'Remember me',
+    'forgot_password' => 'Forgot your password?',
+
     'failed' => 'These credentials do not match our records.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
