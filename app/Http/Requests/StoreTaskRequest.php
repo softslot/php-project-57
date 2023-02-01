eb348223aca051ea68f8be1946b19372a1e7b54c
Konstantin Korolenko <softslot-com@yandex.ru>
2023-02-01 18:55:00 +0300

    add labels to validation

--- app/Http/Requests/StoreTaskRequest.php
+++ app/Http/Requests/StoreTaskRequest.php
@@ -28,6 +28,7 @@ class StoreTaskRequest extends FormRequest
             'description' => 'nullable',
             'status_id' => 'required|exists:task_statuses,id',
             'assigned_to_id' => 'nullable|exists:users,id',
+            'labels' => '',
         ];
     }
 }
