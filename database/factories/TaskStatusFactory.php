2daea177c184a9df988d69ed1013463a386e47a0
Konstantin Korolenko <softslot-com@yandex.ru>
2023-03-11 10:08:55 +0300

    add TaskStatusFactory

---
+++ database/factories/TaskStatusFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TaskStatus;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\TaskStatus>
+ */
+class TaskStatusFactory extends Factory
+{
+    protected $model = TaskStatus::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->text(30),
+        ];
+    }
+}
