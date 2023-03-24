412cfdd6141d343583c7108f7fe8ae1c5a505e0d
Konstantin Korolenko <softslot-com@yandex.ru>
2023-03-24 19:52:43 +0300

    add TaskFactory

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TaskStatus;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $taskStatusesIds = TaskStatus::get('id')->pluck('id');
+        $usersIds = User::get('id')->pluck('id');
+
+        return [
+            'name' => fake()->unique()->text(30),
+            'status_id' => $taskStatusesIds->random(),
+            'description' => fake()->text(100),
+            'created_by_id' => $usersIds->random(),
+            'assigned_to_id' => $usersIds->random(),
+        ];
+    }
+}
