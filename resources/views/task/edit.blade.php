a535b82fbc3e3307b5171d9a1430574decc14a54
Konstantin Korolenko <softslot-com@yandex.ru>
2023-03-24 19:55:27 +0300

    refactoring views/task

--- resources/views/task/edit.blade.php
+++ resources/views/task/edit.blade.php
@@ -2,6 +2,6 @@
     <h1 class="mb-5">{{ __('task.edit') }}</h1>
 
     {{ Form::open(['route' => ['tasks.update', $task->id], 'method' => 'put']) }}
-        @include('task._form', ['buttonText' => __('task.edit')])
+        @include('task._form', ['buttonText' => __('main.update')])
     {{ Form::close() }}
 </x-app-layout>
