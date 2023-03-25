1e96b4a5c86fa20a602b5d7c4b182b1d5baa2a22
Konstantin Korolenko <softslot-com@yandex.ru>
2023-03-25 10:07:00 +0300

    add LabelTest

--- tests/Feature/Http/Controllers/LabelControllerTest.php
+++ tests/Feature/Http/Controllers/LabelControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\Models\Label;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -9,15 +11,52 @@ class LabelControllerTest extends TestCase
 {
     use RefreshDatabase;
 
+    private User $user;
+    private Label $label;
+
     protected function setUp(): void
     {
         parent::setUp();
 
+        $this->user = User::factory()->create();
+        $this->label = Label::factory()->create();
     }
 
     public function test_index_page(): void
     {
         $response = $this->get(route('labels.index'));
+
+        $response->assertOk();
+    }
+
+    public function test_create_page_from_guest(): void
+    {
+        $response = $this->get(route('labels.create'));
+
+        $response->assertForbidden();
+    }
+
+    public function test_create_page_from_user(): void
+    {
+        $response = $this
+            ->actingAs($this->user)
+            ->get(route('labels.create'));
+
+        $response->assertOk();
+    }
+
+    public function test_edit_page_from_guest(): void
+    {
+        $response = $this->get(route('labels.edit', $this->label->id));
+
+        $response->assertForbidden();
+    }
+
+    public function test_edit_page_from_user(): void
+    {
+        $response = $this
+            ->actingAs($this->user)
+            ->get(route('labels.edit', $this->label->id));
 
         $response->assertOk();
     }
