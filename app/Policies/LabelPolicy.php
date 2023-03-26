d0b823ed285555dc142622db2e21bddff2bb1063
Konstantin Korolenko <softslot-com@yandex.ru>
2023-03-26 13:56:20 +0300

    refactoring policies

--- app/Policies/LabelPolicy.php
+++ app/Policies/LabelPolicy.php
@@ -5,65 +5,33 @@ namespace App\Policies;
 use App\Models\Label;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
 
 class LabelPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function viewAny(?User $user)
+    public function viewAny(?User $user): Response|bool
     {
         return true;
     }
 
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Label  $label
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function view(User $user, Label $label)
+    public function view(User $user, Label $label): Response|bool
     {
         return true;
     }
 
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function create(User $user)
+    public function create(User $user): Response|bool
     {
         return true;
     }
 
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Label  $label
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function update(User $user, Label $label)
+    public function update(User $user, Label $label): Response|bool
     {
         return true;
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Label  $label
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function delete(User $user, Label $label)
+    public function delete(User $user, Label $label): Response|bool
     {
         return true;
     }
--- app/Policies/TaskStatusPolicy.php
+++ app/Policies/TaskStatusPolicy.php
@@ -5,53 +5,28 @@ namespace App\Policies;
 use App\Models\TaskStatus;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
 
 class TaskStatusPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function viewAny(?User $user)
+    public function viewAny(?User $user): Response|bool
     {
         return true;
     }
 
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function create(User $user)
+    public function create(User $user): Response|bool
     {
         return true;
     }
 
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\TaskStatus  $taskStatus
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function update(User $user, TaskStatus $taskStatus)
+    public function update(User $user, TaskStatus $taskStatus): Response|bool
     {
         return true;
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\TaskStatus  $taskStatus
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function delete(User $user, TaskStatus $taskStatus)
+    public function delete(User $user, TaskStatus $taskStatus): Response|bool
     {
         return true;
     }
