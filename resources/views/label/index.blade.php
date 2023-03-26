4ab5bc514f3de615ef1dda7f83344935bdbb810e
Konstantin Korolenko <softslot-com@yandex.ru>
2023-03-26 15:30:48 +0300

    refactoring view/components/table

--- resources/views/label/index.blade.php
+++ resources/views/label/index.blade.php
@@ -10,7 +10,7 @@
     @endcan
 
     <x-table>
-        <x-slot name="thead">
+        <x-table.thead>
             <x-table.tr>
                 <x-table.th class="text-center">{{ __('label.id') }}</x-table.th>
                 <x-table.th>{{ __('label.name') }}</x-table.th>
@@ -20,31 +20,33 @@
                     <x-table.th class="text-center">{{ __('table.actions') }}</x-table.th>
                 @endauth
             </x-table.tr>
-        </x-slot>
+        </x-table.thead>
+        
+        <x-table.tbody>
+            @foreach ($labels as $label)
+                <x-table.tr>
+                    <x-table.td class="text-center">{{ $label->id }}</x-table.td>
+                    <x-table.td>{{ $label->name }}</x-table.td>
+                    <x-table.td>{{ $label->description }}</x-table.td>
+                    <x-table.td>{{ $label->created_at }}</x-table.td>
+                    @auth
+                        <x-table.td class="space-x-3 text-center">
+                            @can('delete', $label)
+                                <a data-confirm="{{ __('table.confirm') }}"
+                                    data-method="delete"
+                                    class="text-red-600 hover:text-red-900"
+                                    href="{{ route('labels.destroy', $label->id) }}">{{ __('table.delete') }}</a>
+                            @endcan
 
-        @foreach ($labels as $label)
-            <x-table.tr>
-                <x-table.td class="text-center">{{ $label->id }}</x-table.td>
-                <x-table.td>{{ $label->name }}</x-table.td>
-                <x-table.td>{{ $label->description }}</x-table.td>
-                <x-table.td>{{ $label->created_at }}</x-table.td>
-                @auth
-                    <x-table.td class="space-x-3 text-center">
-                        @can('delete', $label)
-                            <a data-confirm="{{ __('table.confirm') }}"
-                                data-method="delete"
-                                class="text-red-600 hover:text-red-900"
-                                href="{{ route('labels.destroy', $label->id) }}">{{ __('table.delete') }}</a>
-                        @endcan
-
-                        @can('update', $label)
-                            <a class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                                href="{{ route('labels.edit', $label->id) }}">{{ __('table.edit') }}</a>
-                        @endcan
-                    </x-table.td>
-                @endauth
-            </x-table.tr>
-        @endforeach
+                            @can('update', $label)
+                                <a class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                                    href="{{ route('labels.edit', $label->id) }}">{{ __('table.edit') }}</a>
+                            @endcan
+                        </x-table.td>
+                    @endauth
+                </x-table.tr>
+            @endforeach
+        </x-table.tbody>
     </x-table>
 
     <div class="mt-4">
