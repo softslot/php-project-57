f75d44a8eb5575a7573ddcd1cbae53326eaee273
Konstantin Korolenko <softslot-com@yandex.ru>
2023-03-28 19:45:43 +0300

    fix

--- resources/views/pages/labels/edit.blade.php
+++ resources/views/pages/labels/edit.blade.php
@@ -2,6 +2,6 @@
     <h1 class="mb-5">{{ __('label.edit') }}</h1>
 
     {{ Form::open(['route' => ['labels.update', $label->id], 'method' => 'put']) }}
-        @include('pages.labels._form', ['buttonText' => __('main.edit')])
+        @include('pages.labels._form', ['buttonText' => __('main.update')])
     {{ Form::close() }}
 </x-app-layout>
