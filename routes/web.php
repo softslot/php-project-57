01a1b35ad2360db0f537c160490abbd30b172b3c
Konstantin Korolenko <softslot-com@yandex.ru>
2023-03-28 14:44:40 +0300

    remove profile routes

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,16 @@
 <?php
 
 use App\Http\Controllers\LabelController;
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TaskStatusController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'pages.home')->name('home');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 Route::resource('task_statuses', TaskStatusController::class)->except(['show']);
+
 Route::resource('tasks', TaskController::class);
+
 Route::resource('labels', LabelController::class)->except(['show']);
 
 require __DIR__ . '/auth.php';
