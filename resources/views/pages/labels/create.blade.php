1dda1a0a71e8d4c246b9760b4aae5653f60042e1
Konstantin Korolenko <softslot-com@yandex.ru>
2023-03-28 19:44:56 +0300

    fix lang;

--- resources/views/pages/labels/create.blade.php
+++ resources/views/pages/labels/create.blade.php
@@ -2,6 +2,6 @@
     <h1 class="mb-5">{{ __('label.create') }}</h1>
 
     {{ Form::open(['route' => 'labels.store', 'method' => 'post']) }}
-        @include('pages.labels._form', ['buttonText' => __('label.create')])
+        @include('pages.labels._form', ['buttonText' => __('main.create')])
     {{ Form::close() }}
 </x-app-layout>
--- resources/views/pages/labels/edit.blade.php
+++ resources/views/pages/labels/edit.blade.php
@@ -2,6 +2,6 @@
     <h1 class="mb-5">{{ __('label.edit') }}</h1>
 
     {{ Form::open(['route' => ['labels.update', $label->id], 'method' => 'put']) }}
-        @include('pages.labels._form', ['buttonText' => __('label.edit')])
+        @include('pages.labels._form', ['buttonText' => __('main.edit')])
     {{ Form::close() }}
 </x-app-layout>
